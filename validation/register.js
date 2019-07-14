08ce620766609eca29b93b84f05fa89c2817f9dd
Rina Yang <42502822+didflsk@users.noreply.github.com>
2019-07-14 04:31:11 -0700

    tasks #13-17

---
+++ validation/register.js
@@ -0,0 +1,37 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegisterInput
+(data){
+  let errors = {};
+
+data.name = !isEmpty(data.name)? data.name : '';
+data.email = !isEmpty(data.email)? data.email : '';
+data.password = !isEmpty(data.password)? data.password : '';
+data.password2 = !isEmpty(data.password2)? data.password2 : '';
+
+  if (!validator.isLength(data.name, {min: 2, max: 30})){errors.name ='Name must between 2 and 30 characters';
+  }
+
+  if (!validator.isLength(data.password, {min: 6, max: 30})){errors.name ='Password must between 6 and 30 characters';
+  }
+
+  if (validator.isEmpty(data.name)){errors.name ='Name field is required';
+  }
+
+  if (validator.isEmpty(data.email)){errors.name ='Email field is required';
+  }
+
+  if (validator.isEmpty(data.password)){errors.name ='Password field is required';
+  }
+
+  if (validator.isEmpty(data.password2)){errors.name ='Confirmation password field is required';
+  }
+
+  if  (!validator.equals(data.password, data.password2)){errors.password2='Password must match;'
+  }
+  return {
+  errors,
+    isValid: isEmpty(errors)
+  }
+}
