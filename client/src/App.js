59070969345bab01987ac4ecd8d6e7e98ff18ee1
Rina Yang <42502822+didflsk@users.noreply.github.com>
2019-08-04 15:19:44 -0700

    finished up to module 19

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,30 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import store from './store';
 import {Provider} from 'react-redux';
+import setAuthToken from './utils/setAuthtoken';
+import jwt_decode from 'jwt-decode';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+//Check for token
+if (localStorage.jwtToken){
+  // set auth token header
+  setAuthToken(localStorage.jwtToken);
+  // Decode
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user is authenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    //Clear current profile
+
+    //Redirect the user to login
+    window.location.href = '/login'
+  } 
+}
 
 function App() {
   return (
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import {TEST_DISPATCH, GET_ERRORS} from './types';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthtoken';
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (userData, history) => 
 dispatch => {
@@ -13,9 +15,44 @@ dispatch => {
 
     })
     );
+}
 
+export const setCurrentUser = decoded => {
   return {
-    type: TEST_DISPATCH,
-    payload: userData
+    type: SET_CURRENT_USER,
+    payload: decoded
   }
+}
+
+export const loginUser = userdata => dispatch => {
+  axios
+      .post('/api/users/login', userdata)
+      .then(res => {
+        //Save to localstorage
+        const {token} = res.data;
+        //set token to ls
+        localStorage.setItem('jwtToken', token);
+        //set token to auth header
+        setAuthToken(token);
+        //Decode token to get user data
+        const decoded = jwt_decode(token);
+        //set current user
+        dispatch(setCurrentUser(decoded))
+      })
+      .catch(err => 
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+    
+        })
+        );
+}
+
+export const logoutUser = () => dispatch => {
+  //Remove token from localstorage
+  localStorage.removeItem('jwtToken');
+  //Remove token from auth headers
+  setAuthToken(false);
+  //Clean the user data from redux store
+  dispatch(setCurrentUser({}));
 }
