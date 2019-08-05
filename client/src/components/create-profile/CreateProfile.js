6198e0a343d979013b8570676e896064aa10c712
Rachel Salazar <ret1992@gmail.com>
2019-08-04 18:22:28 -0700

    getting rid of experience

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -4,8 +4,8 @@ import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
-import InputGroup from '../common/InputGroup';
-import SelectListGroup from '../common/SelectListGroup';
+//import InputGroup from '../common/InputGroup';
+//import SelectListGroup from '../common/SelectListGroup';
 import { createProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
@@ -14,18 +14,18 @@ class CreateProfile extends Component {
     this.state = {
       displaySocialInputs: false,
       handle: '',
-      company: '',
-      website: '',
-      location: '',
-      status: '',
-      skills: '',
-      githubusername: '',
+      //company: '',
+      //website: '',
+      //location: '',
+      //status: '',
+      //skills: '',
+      //githubusername: '',
       bio: '',
-      twitter: '',
-      facebook: '',
-      linkedin: '',
-      youtube: '',
-      instagram: '',
+      //twitter: '',
+      //facebook: '',
+      //linkedin: '',
+      //youtube: '',
+      //instagram: '',
       errors: {}
     };
 
@@ -44,18 +44,18 @@ class CreateProfile extends Component {
 
     const profileData = {
       handle: this.state.handle,
-      company: this.state.company,
-      website: this.state.website,
-      location: this.state.location,
-      status: this.state.status,
-      skills: this.state.skills,
-      githubusername: this.state.githubusername,
+      // company: this.state.company,
+      // website: this.state.website,
+      // location: this.state.location,
+      // status: this.state.status,
+      // skills: this.state.skills,
+      // githubusername: this.state.githubusername,
       bio: this.state.bio,
-      twitter: this.state.twitter,
-      facebook: this.state.facebook,
-      linkedin: this.state.linkedin,
-      youtube: this.state.youtube,
-      instagram: this.state.instagram
+      // twitter: this.state.twitter,
+      // facebook: this.state.facebook,
+      // linkedin: this.state.linkedin,
+      // youtube: this.state.youtube,
+      // instagram: this.state.instagram
     };
 
     this.props.createProfile(profileData, this.props.history);
@@ -68,71 +68,71 @@ class CreateProfile extends Component {
   render() {
     const { errors, displaySocialInputs } = this.state;
 
-    let socialInputs;
+    // let socialInputs;
 
-    if (displaySocialInputs) {
-      socialInputs = (
-        <div>
-          <InputGroup
-            placeholder="Twitter Profile URL"
-            name="twitter"
-            icon="fab fa-twitter"
-            value={this.state.twitter}
-            onChange={this.onChange}
-            error={errors.twitter}
-          />
+    // if (displaySocialInputs) {
+    //   socialInputs = (
+    //     <div>
+    //       <InputGroup
+    //         placeholder="Twitter Profile URL"
+    //         name="twitter"
+    //         icon="fab fa-twitter"
+    //         value={this.state.twitter}
+    //         onChange={this.onChange}
+    //         error={errors.twitter}
+    //       />
 
-          <InputGroup
-            placeholder="Facebook Page URL"
-            name="facebook"
-            icon="fab fa-facebook"
-            value={this.state.facebook}
-            onChange={this.onChange}
-            error={errors.facebook}
-          />
+    //       <InputGroup
+    //         placeholder="Facebook Page URL"
+    //         name="facebook"
+    //         icon="fab fa-facebook"
+    //         value={this.state.facebook}
+    //         onChange={this.onChange}
+    //         error={errors.facebook}
+    //       />
 
-          <InputGroup
-            placeholder="Linkedin Profile URL"
-            name="linkedin"
-            icon="fab fa-linkedin"
-            value={this.state.linkedin}
-            onChange={this.onChange}
-            error={errors.linkedin}
-          />
+    //       <InputGroup
+    //         placeholder="Linkedin Profile URL"
+    //         name="linkedin"
+    //         icon="fab fa-linkedin"
+    //         value={this.state.linkedin}
+    //         onChange={this.onChange}
+    //         error={errors.linkedin}
+    //       />
 
-          <InputGroup
-            placeholder="YouTube Channel URL"
-            name="youtube"
-            icon="fab fa-youtube"
-            value={this.state.youtube}
-            onChange={this.onChange}
-            error={errors.youtube}
-          />
+    //       <InputGroup
+    //         placeholder="YouTube Channel URL"
+    //         name="youtube"
+    //         icon="fab fa-youtube"
+    //         value={this.state.youtube}
+    //         onChange={this.onChange}
+    //         error={errors.youtube}
+    //       />
 
-          <InputGroup
-            placeholder="Instagram Page URL"
-            name="instagram"
-            icon="fab fa-instagram"
-            value={this.state.instagram}
-            onChange={this.onChange}
-            error={errors.instagram}
-          />
-        </div>
-      );
-    }
+    //       <InputGroup
+    //         placeholder="Instagram Page URL"
+    //         name="instagram"
+    //         icon="fab fa-instagram"
+    //         value={this.state.instagram}
+    //         onChange={this.onChange}
+    //         error={errors.instagram}
+    //       />
+    //     </div>
+    //   );
+    // }
 
     // Select options for status
-    const options = [
-      { label: '* Select Professional Status', value: 0 },
-      { label: 'Developer', value: 'Developer' },
-      { label: 'Junior Developer', value: 'Junior Developer' },
-      { label: 'Senior Developer', value: 'Senior Developer' },
-      { label: 'Manager', value: 'Manager' },
-      { label: 'Student or Learning', value: 'Student or Learning' },
-      { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
-      { label: 'Intern', value: 'Intern' },
-      { label: 'Other', value: 'Other' }
-    ];
+    // const options = [
+    //   { label: '* Select Professional Status', value: 0 },
+    //   { label: 'Developer', value: 'Developer' },
+    //   { label: 'Junior Developer', value: 'Junior Developer' },
+    //   { label: 'Senior Developer', value: 'Senior Developer' },
+    //   { label: 'Manager', value: 'Manager' },
+    //   { label: 'Student or Learning', value: 'Student or Learning' },
+    //   { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
+    //   { label: 'Intern', value: 'Intern' },
+    //   { label: 'Other', value: 'Other' }
+    // ];
 
     return (
       <div className="create-profile">
@@ -153,7 +153,7 @@ class CreateProfile extends Component {
                   error={errors.handle}
                   info="A unique handle for your profile URL. Your full name, company name, nickname"
                 />
-                <SelectListGroup
+                {/* <SelectListGroup
                   placeholder="Status"
                   name="status"
                   value={this.state.status}
@@ -161,32 +161,32 @@ class CreateProfile extends Component {
                   options={options}
                   error={errors.status}
                   info="Give us an idea of where you are at in your career"
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="Company"
                   name="company"
                   value={this.state.company}
                   onChange={this.onChange}
                   error={errors.company}
                   info="Could be your own company or one you work for"
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="Website"
                   name="website"
                   value={this.state.website}
                   onChange={this.onChange}
                   error={errors.website}
                   info="Could be your own website or a company one"
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="Location"
                   name="location"
                   value={this.state.location}
                   onChange={this.onChange}
                   error={errors.location}
                   info="City or city & state suggested (eg. Boston, MA)"
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="* Skills"
                   name="skills"
                   value={this.state.skills}
@@ -194,15 +194,15 @@ class CreateProfile extends Component {
                   error={errors.skills}
                   info="Please use comma separated values (eg.
                     HTML,CSS,JavaScript,PHP"
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="Github Username"
                   name="githubusername"
                   value={this.state.githubusername}
                   onChange={this.onChange}
                   error={errors.githubusername}
                   info="If you want your latest repos and a Github link, include your username"
-                />
+                /> */}
                 <TextAreaFieldGroup
                   placeholder="Short Bio"
                   name="bio"
@@ -212,7 +212,7 @@ class CreateProfile extends Component {
                   info="Tell us a little about yourself"
                 />
 
-                <div className="mb-3">
+                {/* <div className="mb-3">
                   <button
                     type="button"
                     onClick={() => {
@@ -226,7 +226,7 @@ class CreateProfile extends Component {
                   </button>
                   <span className="text-muted">Optional</span>
                 </div>
-                {socialInputs}
+                {socialInputs} */}
                 <input
                   type="submit"
                   value="Submit"
